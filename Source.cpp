3cd6d84a2f01e6990df6c06da949539547595011
Bach Phung <bach.phung@tuni.fi>
2021-11-23 16:56:50 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <climits>
 #include"Airport.h"
 #include"Airport_1.h"
 #include"Airport_2.h"
@@ -7,6 +8,44 @@
 #include"Airport_5.h"
 
 using namespace std;
+
+int chooseVersion()
+{
+    int userChoise;
+
+    bool acceptable = false;
+
+    while (true)
+    {
+
+        cout << "Welcome to airport simulator.\n "
+            << "Please choose the version you would like to run\n"
+            << "1: task 1: single shared runway\n"
+            << "2: task 2: separate runways for arrival and departure\n"
+            << "3: task 3: separate runways for arrival and departure, planes can be moved between\n"
+            << "4: task 4: three runways, two always for arrival and departure, one primary for arrival \n"
+            << "5: task 5: single shared runway, each arrival plane has its own fuel.\n"
+            << "Input: "
+            << flush;
+
+        if (cin >> userChoise)
+        {
+            if (userChoise == 1 || userChoise == 2 || userChoise == 3 || userChoise == 4 || userChoise == 5)
+            {
+                return userChoise;
+            }
+        }
+        else
+        {
+            // clear error from cin so it works correctly when asking again.
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
+        }
+    }
+
+    return userChoise;
+}
+
 
 int main()     //  Airport simulation program
 /*
@@ -21,12 +60,28 @@ Uses: Classes Runway, Plane, Random and functions run_idle, initialize.
 */
 
 {   
-    Airport* airport = new Airport_5();
+    int version = chooseVersion();
+    Airport* airport = NULL ;
+    switch (version)
+    {
+    case 1:
+        airport = new Airport_1();
+        break;
+    case 2:
+        airport = new Airport_2();
+        break;
+    case 3:
+        airport = new Airport_3();
+        break;
+    case 4:
+        airport = new Airport_4();
+        break;
+    case 5:
+        airport = new Airport_5();
+        break;
+    }
     airport->run();
     airport->shut_down();
-    
-    
-    
-
+    delete airport;
     return 0;
 }
